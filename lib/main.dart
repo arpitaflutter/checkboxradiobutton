7d0c306db5acac05beb1086264735ef2c755cad5
arpitaflutter <arpitamangukiya2000@gmail.com>
2023-01-20 10:58:54 +0530

    19/1/2023 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main()
@@ -27,50 +29,62 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Text("Languages",style: TextStyle(color: Colors.indigo,fontSize: 20,fontWeight: FontWeight.bold),),
-            Center(
-              child: CheckboxListTile(
-                value: lang1,
-                title: Text("Gujarati"),
-                activeColor: Colors.indigo,
-                onChanged: (value) {
-                  setState(() {
-                    lang1 = value!;
-                  });
-                },
+    return WillPopScope(
+      onWillPop: () => back(),
+      child: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Text("Languages",style: TextStyle(color: Colors.indigo,fontSize: 20,fontWeight: FontWeight.bold),),
+              Center(
+                child: CheckboxListTile(
+                  value: lang1,
+                  title: Text("Gujarati"),
+                  activeColor: Colors.indigo,
+                  onChanged: (value) {
+                    setState(() {
+                      lang1 = value!;
+                    });
+                  },
+                ),
               ),
-            ),
-            Center(
-              child: CheckboxListTile(
-                value: lang2,
-                title: Text("English"),
-                activeColor: Colors.indigo,
-                onChanged: (value) {
-                  setState(() {
-                    lang2 = value!;
-                  });
-                },
+              Center(
+                child: CheckboxListTile(
+                  value: lang2,
+                  title: Text("English"),
+                  activeColor: Colors.indigo,
+                  onChanged: (value) {
+                    setState(() {
+                      lang2 = value!;
+                    });
+                  },
+                ),
               ),
-            ),
-            Center(
-              child: CheckboxListTile(
-                value: lang3,
-                title: Text("Hindi"),
-                activeColor: Colors.indigo,
-                onChanged: (value) {
-                  setState(() {
-                    lang3 = value!;
-                  });
-                },
+              Center(
+                child: CheckboxListTile(
+                  value: lang3,
+                  title: Text("Hindi"),
+                  activeColor: Colors.indigo,
+                  onChanged: (value) {
+                    setState(() {
+                      lang3 = value!;
+                    });
+                  },
+                ),
               ),
-            ),
-            RadioListTile(
-                value: "Android",
-                title: Text("Android"),
+              RadioListTile(
+                  value: "Android",
+                  title: Text("Android"),
+                  groupValue: selected,
+                  onChanged: (value) {
+                    setState(() {
+                      selected = value!;
+                    });
+                  },
+                ),
+              RadioListTile(
+                value: "IOS",
+                title: Text("IOS"),
                 groupValue: selected,
                 onChanged: (value) {
                   setState(() {
@@ -78,50 +92,84 @@ class _HomeState extends State<Home> {
                   });
                 },
               ),
-            RadioListTile(
-              value: "IOS",
-              title: Text("IOS"),
-              groupValue: selected,
-              onChanged: (value) {
-                setState(() {
-                  selected = value!;
-                });
-              },
-            ),
-            RadioListTile(
-              value: "Web",
-              title: Text("Web"),
-              groupValue: selected,
-              onChanged: (value) {
-                setState(() {
-                  selected = value!;
-                });
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: DropdownButton(
-                        items: [
-                          DropdownMenuItem(child: Text("Surat"),value: "Surat",),
-                          DropdownMenuItem(child: Text("Vapi"),value: "Vapi",),
-                        ],
-                        onChanged: (value) {
-                          setState(() {
-                            data = value!;
-                          });
-                        },
-                      value: data,
-                    ),
-                  ),
-                ],
+              RadioListTile(
+                value: "Web",
+                title: Text("Web"),
+                groupValue: selected,
+                onChanged: (value) {
+                  setState(() {
+                    selected = value!;
+                  });
+                },
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: DropdownButton(
+                          items: [
+                            DropdownMenuItem(child: Text("Surat"),value: "Surat",),
+                            DropdownMenuItem(child: Text("Vapi"),value: "Vapi",),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              data = value!;
+                            });
+                          },
+                        value: data,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+             ElevatedButton(onPressed: () {
+              Exitdialog();
+              }, child: Text("Submit"))
+            ],
+          ),
         ),
       ),
     );
   }
+
+  void Exitdialog()
+  {
+     showDialog(
+       barrierDismissible: false,
+       context: context,
+       builder: (context) {
+         return  AlertDialog(
+           content: SizedBox(
+             height: 80,
+             child: Column(
+               children: [
+                 Text("Are you sure you want to exit?"),
+                 SizedBox(height: 20,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                     ElevatedButton(onPressed: () {
+                       exit(0);
+                     }, child: Text("Yes")),
+                     SizedBox(width: 10,),
+                     ElevatedButton(onPressed: () {
+                       Navigator.pop(context);
+                     }, child: Text("No"))
+                   ],
+                 ),
+               ],
+             ),
+           ),
+         );
+       },
+     );
+  }
+
+  Future<bool> back() async{
+
+    Exitdialog();
+    return await false;
+  }
+
 }
